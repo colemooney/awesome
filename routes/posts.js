998ecca6ee8a6c648a912c06ca43cd8008fddeca
colemooney <colemooney5@gmail.com>
2020-09-12 20:29:45 -0400

    post route to ejs

--- routes/posts.js
+++ routes/posts.js
@@ -16,4 +16,33 @@ router.get('/posts', (req, res, next) => {
     } )
 });
 
+router.get('/posts/:id', (req, res, next)=>{
+    console.log("PARAMS ------------",req.params.id)
+    let id = req.params.id;
+    Post.findById(id)
+    .then((result)=>{
+        console.log("RESULT ----------------", result)
+        Post.find({user: req.params.id})
+        .populate('user')
+        .then((postresult)=>{
+          console.log("postRESULT --------------", postresult)
+          data = {
+            result: result,
+            postresult: postresult
+          }
+          console.log("these are the results ----------- ", result);
+          res.render(`singlePost`, data)
+        })
+        .catch((err)=>{
+          next(err)
+      })
+      .catch((err)=>{
+        next(err)
+      })
+    })
+    })
+
+    
+  
+
 module.exports = router;
