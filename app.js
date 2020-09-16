924e99423159bd01e90195d6e48df56931f2b165
colemooney <colemooney5@gmail.com>
2020-09-16 16:15:16 -0400

    middleware setup

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const bodyParser   = require('body-parser');
 const layouts      = require('express-ejs-layouts');
 const mongoose     = require('mongoose');
 
+const flash        = require("connect-flash");
+const session      = require("express-session");
+
+const MongoStore   = require("connect-mongo")(session);
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+
 mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/coleture');
 
@@ -29,6 +39,85 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(layouts);
+
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.use(flash());
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: "/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // to see the structure of the data in received response:
+      console.log("Google account details:", profile);
+
+      User.findOne({ googleID: profile.id })
+      
+        .then(user => {
+          if (user) {
+            done(null, user);
+            return;
+          }
+          let theImage = "";
+          if(profile.photos){
+            theImage = profile.photos[0].value;
+          }
+            
+          User.create({ googleID: profile.id,
+            isAdmin: false,
+            image: theImage,
+            username: profile._json.name
+          
+          })
+            .then(newUser => {
+              done(null, newUser);
+            })
+            .catch(err => done(err)); // closes User.create()
+        })
+        .catch(err => done(err)); // closes User.findOne()
+    }
+  )
+);
+
 
 const index = require('./routes/index');
 app.use('/', index);
